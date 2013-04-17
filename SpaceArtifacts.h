1595e4c3701d9f814f90c6ff30de930eec7b20f2
mhuss.HP <ff@ff>
2013-04-17 02:13:02 -0400

    s

---
+++ SpaceArtifacts.h
@@ -0,0 +1,17 @@
+/*
+ * SpaceArtifacts.h
+ *
+ *  Created on: Aug 20, 2012
+ *      Author: hp
+ */
+
+#ifndef SPACEARTIFACTS_H_
+#define SPACEARTIFACTS_H_
+
+
+class spaceArtifact
+{
+	void moveArtifact(void);
+};
+
+#endif /* SPACEARTIFACTS_H_ */
